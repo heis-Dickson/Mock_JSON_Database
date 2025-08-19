9d300c057aa54484baa011bac5e0e956683d327e
heis-Dickson <awokiyesimayowa@gmail.com>
2025-08-19 10:48:27 -0600

    commit

--- utils/file.js
+++ utils/file.js
@@ -1,16 +1,25 @@
-const fs = require('fs');
-const filePath = require('../database.json');
+const fs = require("fs").promises;
+const filePath = "./database.json";
+
+async function readData() {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    throw new Error("Error reading data: " + err);
+  }
+}
 
 
-async function readData(data) {
-      try {
-          const data = await fs.readFile(filePath, JSON.stringify(data, null, 2));
-      } catch (error) {
-          throw Error('Error reading data: ' + error.message);
-      }
-      }
+async function writeData(data) {
+  try {
+    const data = await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+  } catch (error) {
+    throw new Error("Error reading data: " + error.message);
+  }
+}
 
-      module.exports = {
-        readData,
-        writeData
-      };
+module.exports = {
+  readData,
+  writeData
+};
